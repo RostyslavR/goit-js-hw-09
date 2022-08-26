276dbd627c2c5697004130c2f60d4a8e4bac0083
unknown <rrmail@ukr.net>
2022-08-26 13:21:10 +0300

    upd 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+refStartbtn = document.querySelector('[data-start]');
+refStopbtn = document.querySelector('[data-stop]');
+let intervalID = null;
 
+refStopbtn.disabled = true;
+refStartbtn.addEventListener('click', startProcess);
+refStopbtn.addEventListener('click', stopProcess); //
+
+function startProcess() {
+  //   refStartbtn.removeEventListener('click', startProcess);
+  //   refStopbtn.addEventListener('click', stopProcess);
+  refStopbtn.disabled = false;
+  refStartbtn.disabled = true;
+  intervalID = setInterval(changeBodyColor, 1000);
+}
+
+function stopProcess() {
+  //   refStopbtn.removeEventListener('click', stopProcess);
+  //   refStartbtn.addEventListener('click', startProcess);
+  clearInterval(intervalID);
+  refStartbtn.disabled = false;
+  refStopbtn.disabled = true;
+}
+
+function changeBodyColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
